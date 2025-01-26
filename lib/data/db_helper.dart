ef7826832066af6a8ef72e8bd84fe950d86ff14d
Rodrigue KASSI <rodrigue.kassi09@gmail.com>
2025-01-26 21:25:11 +0000

    Ajout de la class DbHelper pour la communication avec la base de donnée

---
+++ lib/data/db_helper.dart
@@ -0,0 +1,50 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+
+class DbHelper {
+  static Database? _database;
+
+  static Future<Database> getDatabase() async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  static Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'todo_app.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE todo (id INTEGER PRIMARY KEY, task TEXT, done INTEGER)'
+        );
+      },
+    );
+  }
+
+  // INSERT DATA
+  static Future<int> insertData(String task) async {
+    final db = await getDatabase();
+    return await db.insert('todo', {'task': task, 'done': 0 });
+  }
+
+  // UPDATE DATA
+  static Future<int> updateData(int id, int done) async {
+    final db = await getDatabase();
+    return await db.update('todo', {'done': done}, where: 'id=?', whereArgs: [id]);
+  }
+
+  // DELETE DATA
+  static Future<int> deleteData(int id) async {
+    final db = await getDatabase();
+    return await db.delete('todo', where: 'id=?', whereArgs: [id]);
+  }
+
+  // LIST ALL DATA
+  static Future<List<Map<String, dynamic>>> getAllData() async {
+    final db = await getDatabase();
+    return await db.query('todo');
+  }
+}
